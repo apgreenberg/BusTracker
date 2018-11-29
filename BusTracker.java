93db860a67852480ffcb100f073f9cc034101421
apgreenberg <43008006+apgreenberg@users.noreply.github.com>
2018-11-28 20:40:39 -0600

    Update BusTracker.java

--- BusTracker.java
+++ BusTracker.java
@@ -1,2 +1,25 @@
 public class BusTracker {
+   void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(final VolleyError error) {
+                            Log.w(TAG, error.toString());
+                        }
+                    });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
